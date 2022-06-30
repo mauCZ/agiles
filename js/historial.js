180ede5a740b5662cc297b1c34660cfd3b4ee774
ronald <roaldtp@gmail.com>
2022-06-30 20:28:40 -0300

    pbi parcialmente terminado

--- js/historial.js
+++ js/historial.js
@@ -1,11 +1,11 @@
-
-
 $(function () {
   let contenido = $(".table_body");
   console.log(contenido);
   contenido.each(function () {
-    $(this).append("<tr><td>ronda 1</td><td>2027-05-02</td><td>Facil</td> <td>10</td><td>8</td> </tr>");
+    for (let i = 0; i < 50; i++) {
+      $(this).append(
+        `<tr><td>ronda 1</td><td>2027-05-02</td><td>Facil</td> <td>${i}</td><td>15</td> </tr>`
+      );
+    }
   });
 });
-
-
