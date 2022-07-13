f0cd1e498baa2fc73fdddd1dabd24ed82b97e939
Víctor Apaza Choque <@victorapazac12345>
2022-07-13 11:18:37 -0400

    ranking con datos

--- js/historial.js
+++ js/historial.js
@@ -1,11 +1,25 @@
+let cerrarSesion=document.getElementById('cerrarSesion');
 $(function () {
-  let contenido = $(".table_body");
-  console.log(contenido);
-  contenido.each(function () {
+  let contenidoDeLaTablaHistorial = $(".table_body");
+  const userName = sessionStorage.getItem("username");
+  const templateUser = `
+                        <i>${userName}</i>
+                      `;
+  const createTemplateUser = (document.getElementById("box-sesion").innerHTML =
+    templateUser);
+  
+  contenidoDeLaTablaHistorial.each(function () {
     for (let i = 0; i < 50; i++) {
       $(this).append(
-        `<tr><td>ronda 1</td><td>2027-05-02</td><td>Facil</td> <td>${i}</td><td>15</td> </tr>`
+        `<tr><td>ronda 1</td><td>2022-07-12</td><td>Facil</td> <td>${i}</td><td>15</td> </tr>`
       );
     }
   });
 });
+
+cerrarSesion.addEventListener('click',() => {
+  console.log('cerando sesion')
+  window.location.replace("../index.html");
+  sessionStorage.clear()
+})
+
--- js/ranking.js
+++ js/ranking.js
@@ -1,12 +1,26 @@
+let cerrarSesion=document.getElementById('cerrarSesion');
 $(function () {
-    let contenido = $(".table_body");
-    console.log(contenido);
-    contenido.each(function () {
-      for (let i = 0; i < 50; i++) {
-        $(this).append(
-          `<tr><td>Usuario</td> <td>${i}</td> </tr>`
-        );
-      }
-    });
-  });
+  let contenidoDeLaTablaHistorial = $(".table_body");
+  const userName = sessionStorage.getItem("username");
+  const templateUser = `
+                        <i>${userName}</i>
+                      `;
+  const createTemplateUser = (document.getElementById("box-sesion").innerHTML =
+    templateUser);
   
+  contenidoDeLaTablaHistorial.each(function () {
+    for (let i = 1; i < 5; i++) {
+      $(this).append(
+        `<tr><td>${i}</td><td>Mauricio</td> <td>50</td><td>30%</td> </tr>`
+      );
+    }
+
+  });
+});
+
+cerrarSesion.addEventListener('click',() => {
+  console.log('cerando sesion')
+  window.location.replace("../index.html");
+  sessionStorage.clear()
+})
+
